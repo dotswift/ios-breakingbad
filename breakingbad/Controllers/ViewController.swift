379daed5c724e5df7440f616f668a31e9127578e
derek quinn <dereq@Troy.local>
2020-11-30 01:59:27 -0500

    Added all character names to UITableView

--- breakingbad/Controllers/ViewController.swift
+++ breakingbad/Controllers/ViewController.swift
@@ -1,18 +1,54 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var nameTable: UITableView!
+    var characterNameList: [String] = []
+    var characters: [Character] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureNameTable()
         
         BreakingBadService.getCharacters(parameters: BreakingBadApiConstants.paramCharacters, characterId: nil, completion: { response in
             
+            self.characters = response
+            
+            for i in 0...response.count-1 {
+                
+                self.characterNameList.append(self.characters[i].name)
+                print(self.characterNameList[i])
+            }
             print("viewDidLoad() [Character].count == ", response.count)
+            
         })
         
         BreakingBadService.getCharacters(parameters: BreakingBadApiConstants.paramCharacters, characterId: "9", completion: { response in
             
             print("viewDidLoad() Character nickname \(response[0].nickname)")
         })
+    }
+    
+    @IBAction func refreshTable() {
+        self.nameTable.reloadData()
+        
+    }
+    
+    // Configure Table View containing names of all characters
+    private func configureNameTable() {
+        nameTable.delegate = self
+        nameTable.dataSource = self
+    }
+    
+    // Determines how many rows are in the Table View
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return characterNameList.count
+    }
+    
+    // Put characters name in each cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = nameTable.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = characterNameList[indexPath.row]
+        return cell
     }
 }
